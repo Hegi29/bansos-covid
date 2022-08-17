4adfb083c64a2b0f42f90a24d2129b637eac7e47
Hegi_29 <hegi@radyalabs.com>
2022-08-17 09:28:32 +0700

    fix: remove unused component

--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -1,24 +1,10 @@
-import { useState } from 'react';
-
 import { FormBansos, MainLayout } from '~/components/.';
-import { ModalPreview } from '~/components/ModalPreview';
 
 const Form = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [previewData, setPreviewData] = useState({});
-
-  const handleModal = () => {
-    setShowModal(true);
-  }
-
-  const handlePreviewData = (data: any) => {
-    setPreviewData(data);
-  }
 
   return (
     <MainLayout page="Form">
-      <FormBansos setShowModal={handleModal} handlePreviewData={handlePreviewData} />
-      <ModalPreview showModal={showModal} setShowModal={handleModal} closeModal={() => setShowModal(false)} props={previewData} />
+      <FormBansos />
     </MainLayout>
   )
 }
